b8dcfc96590d37eb35f0f9392bd3f194b970f986
anguslauhk <62167374+anguslauhk@users.noreply.github.com>
2021-01-14 23:45:27 +0800

    Date & topbar changed

--- docs/start.js
+++ docs/start.js
@@ -16,7 +16,7 @@ var link1img = document.createElement("img");
 link1img.id = "navicon";
 link1img.src = icon;
 link1.appendChild(link1img);
-link1.style = "width:60px;height:100%;padding:0";
+link1.style = "width:60px;height:50px;padding:0";
 topbar.appendChild(link1);
 var navList = [["Home","home.html"],["About Us","about.html"],["Projects","projects.html"],["Products","products.html"]];
 navList.forEach(function(item){
@@ -33,7 +33,7 @@ var footer = document.getElementById("footer");
 var img1 = document.createElement("img");img1.id = "footerimg1";
 footer.appendChild(img1);img1.src = "https://www.hkbrda.org/infolink/output/news-big.gif";
 var p1 = document.createElement("p");footer.appendChild(p1);p1.style = "position:absolute;padding:0;left:20px;width:300px"
-p1.innerHTML = 'Optimum Tech Roblox 2021<br>All rights reserved<br><span style="color:grey;font-size:12px">Last updated on 14 Jan 2021</span>'
+p1.innerHTML = 'Optimum Tech Roblox 2021<br>All rights reserved<br><span style="color:grey;font-size:12px">Last updated on 15 Jan 2021</span>'
 
 var content = document.getElementById("content");
 function posfooter(){var num = window.innerHeight - 310 + "px";content.style.minHeight = num}
